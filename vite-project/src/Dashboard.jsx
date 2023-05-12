6694d315bb51de57820e3049fbfe4bb2f27d58be
unknown <romyguibelondo@gmail.com>
2023-05-12 15:53:09 +0800

    change name in sidebar

--- vite-project/src/Dashboard.jsx
+++ vite-project/src/Dashboard.jsx
@@ -29,7 +29,7 @@ function Dashboard() {
                                     <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                                     </svg>
-                                    <span>Home</span>
+                                    <span>Dashboard</span>
                                 </a>
                             </li>
                             <li className="rounded-sm">
@@ -37,7 +37,7 @@ function Dashboard() {
                                     <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
                                     </svg>
-                                    <span>Inbox</span>
+                                    <span>Manage Employees</span>
                                 </a>
                             </li>
                             <li className="rounded-sm">
@@ -45,7 +45,7 @@ function Dashboard() {
                                     <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
                                     </svg>
-                                    <span>Orders</span>
+                                    <span>Profile</span>
                                 </a>
                             </li>
                             <li className="rounded-sm">
@@ -69,8 +69,10 @@ function Dashboard() {
                     </div>
                 </div>
             </div>
-            <div className="container mx-auto mt-12">
-                <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quae laudantium tempore eius ea possimus? Doloribus earum repudiandae consequuntur quisquam ipsa velit optio architecto nihil harum!</h1>
+            <div className="container mx-auto mt-5 ">
+              <div className="p-2 grid justify-items-centerr">
+                <h4>Employee Management System</h4>
+              </div>
             </div>
         </div>
     );
