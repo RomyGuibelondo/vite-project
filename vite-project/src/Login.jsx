a2fe50995055f687203ae92f84bafa7f73d7d274
Romy Guibelondo <112987484+RomyGuibelondo@users.noreply.github.com>
2023-05-13 21:31:37 +0800

    design login template

--- vite-project/src/Login.jsx
+++ vite-project/src/Login.jsx
@@ -3,6 +3,8 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 function Login() {
+    const [backgroundColor, setBackgroundColor] = useState("#ffffff");
+  
     const [value, setValue] = useState({
         email: "",
         password: "",
@@ -24,25 +26,32 @@ function Login() {
             })
             .catch((err) => console.log(err));
     };
+    const handleColorChange = (event) => {
+        const color = event.target.value;
+        setBackgroundColor(color);
+    };
     return (
-        <div>
-            <div>
-                <div>{error && error} </div>
-                <h2>Login</h2>
-                <form onSubmit={handleSubmit}>
-                    <div>
-                        <label htmlFor="email">Email</label>
-                        <input onChange={(e) => setValue({ ...value, email: e.target.value })} type="email" placeholder="Enter Email" name="email" />
-                    </div>
-                    <div>
-                        <label htmlFor="password">Password</label>
-                        <input onChange={(e) => setValue({ ...value, password: e.target.value })} type="password" placeholder="Enter Password" name="password" />
-                    </div>
-                    <button type="submit">Log in</button>
-                    <p>You are agree to aour terms and policies</p>
-                </form>
+        <>
+            <div style={{backgroundColor}} className="h-screen grid content-center justify-center dark:text-white ">
+            <input className="absolute" type="color" value={backgroundColor} onChange={handleColorChange} />      
+                <div className="w-full  gap-5 grid text-xl bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 p-6 dark:border-gray-700 ">
+                    <div>{error && error} </div>
+                    <h2 className="justify-self-center">Login</h2>
+                    <form className="gap-5 grid" onSubmit={handleSubmit}>
+                        <div>
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="email">Email</label>
+                            <input className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(e) => setValue({ ...value, email: e.target.value })} type="email" placeholder="Enter Email" name="email" />
+                        </div>
+                        <div>
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="password">Password</label>
+                            <input className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(e) => setValue({ ...value, password: e.target.value })} type="password" placeholder="Enter Password" name="password" />
+                        </div>
+                        <button className="font-medium text-primary-600 hover:underline dark:text-primary-500" type="submit">Log in</button>
+                        <p>You are agree to aour terms and policies</p>
+                    </form>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
