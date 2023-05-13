ec63268db0dfac82c60d2126cfbe61f6e21b4f8a
Romy Guibelondo <112987484+RomyGuibelondo@users.noreply.github.com>
2023-05-13 22:51:33 +0800

    adding registration page

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import './App.css'
 import Login from './Login';
-
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Dashboard from './Dashboard'
+import Registration from './Registration';
 
 function App() {
   return (
@@ -12,6 +11,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Dashboard/>}></Route>
       <Route path='/login' element={<Login/>}></Route>
+      <Route path='/registration' element={<Registration/>}></Route>
     </Routes>
     </BrowserRouter>
   )
--- vite-project/src/Login.jsx
+++ vite-project/src/Login.jsx
@@ -4,13 +4,13 @@ import { useNavigate } from "react-router-dom";
 
 function Login() {
     const [backgroundColor, setBackgroundColor] = useState("#ffffff");
-  
+
     const [value, setValue] = useState({
         email: "",
         password: "",
     });
-    
-    const navigate = useNavigate()
+
+    const navigate = useNavigate();
 
     const [error, setError] = useState("");
     const handleSubmit = (event) => {
@@ -19,7 +19,7 @@ function Login() {
             .post("http://localhost:8081/login", value)
             .then((res) => {
                 if (res.data.Status === "Success") {
-                    navigate('/')
+                    navigate("/");
                 } else {
                     setError(res.data.Error);
                 }
@@ -32,25 +32,32 @@ function Login() {
     };
     return (
         <>
-            <div style={{backgroundColor}} className="h-screen grid content-center justify-center dark:text-white ">
-            <input className="absolute" type="color" value={backgroundColor} onChange={handleColorChange} />      
-                <div className="w-full  gap-5 grid text-xl bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 p-6 dark:border-gray-700 ">
+            <div style={{ backgroundColor }} className="h-screen grid content-center justify-center bg-blueGray-100 ">
+                <input className="absolute" type="color" value={backgroundColor} onChange={handleColorChange} />
+                <div className="w-full  gap-5 grid text-xl bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-5 dark:bg-gray-800 p-6 dark:border-gray-700 ">
                     <div>{error && error} </div>
                     <h2 className="justify-self-center">Login</h2>
                     <form className="gap-5 grid" onSubmit={handleSubmit}>
                         <div>
-                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="email">Email</label>
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="email">
+                                Email
+                            </label>
                             <input className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(e) => setValue({ ...value, email: e.target.value })} type="email" placeholder="Enter Email" name="email" />
                         </div>
                         <div>
-                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="password">Password</label>
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="password">
+                                Password
+                            </label>
                             <input className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(e) => setValue({ ...value, password: e.target.value })} type="password" placeholder="Enter Password" name="password" />
                         </div>
-                        <button className="font-medium text-primary-600 hover:underline dark:text-primary-500" type="submit">Log in</button>
+                        <button className="font-medium text-primary-600 hover:underline dark:text-primary-500" type="submit">
+                            Log in
+                        </button>
                         <p>You are agree to aour terms and policies</p>
                     </form>
                 </div>
             </div>
+            
         </>
     );
 }
