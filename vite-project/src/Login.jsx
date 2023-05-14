4b27d03ee926b34dc7ac1e482acce69616021f12
Romy Guibelondo <112987484+RomyGuibelondo@users.noreply.github.com>
2023-05-14 14:47:59 +0800

    redesigne login

--- vite-project/src/Login.jsx
+++ vite-project/src/Login.jsx
@@ -32,11 +32,11 @@ function Login() {
     };
     return (
         <>
-            <div style={{ backgroundColor }} className="h-screen grid content-center justify-center bg-blueGray-100 ">
+            <div style={{ backgroundColor }} className="h-screen grid content-center justify-center bg-blueGray-100  ">
                 <input className="absolute" type="color" value={backgroundColor} onChange={handleColorChange} />
-                <div className="w-full  gap-5 grid text-xl bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-5 dark:bg-gray-800 p-6 dark:border-gray-700 ">
-                    <div>{error && error} </div>
-                    <h2 className="justify-self-center">Login</h2>
+                <div className="w-full  gap-5 grid text-xl rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-5 dark:bg-gray-800 p-6 dark:border-gray-700 ">
+                    <div className="text-red-600">{error && error} </div>
+                    <h2 className="justify-self-center text-white">Sign in to your account</h2>
                     <form className="gap-5 grid" onSubmit={handleSubmit}>
                         <div>
                             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="email">
@@ -50,10 +50,10 @@ function Login() {
                             </label>
                             <input className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(e) => setValue({ ...value, password: e.target.value })} type="password" placeholder="Enter Password" name="password" />
                         </div>
-                        <button className="font-medium text-primary-600 hover:underline dark:text-primary-500" type="submit">
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">
                             Log in
                         </button>
-                        <p>You are agree to aour terms and policies</p>
+                        <p className="block mb-2  text-gray-900 dark:text-white">You are agree to aour terms and policies</p>
                     </form>
                 </div>
             </div>
